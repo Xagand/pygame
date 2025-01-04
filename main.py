80d4acdb2a9b7eb331ac847bd1fafc354ce92cfc
Xagand <dariasport21@gmail.com>
2025-01-04 07:50:00 -0600

    Left/Right rotation

--- main.py
+++ main.py
@@ -9,18 +9,23 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     fps = pygame.time.Clock()
     dt = 0
-        
+    
     while True:     #initializing game surface    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        player.draw(screen)
-        pygame.display.flip()
         dt = fps.tick(60) / 1000
+        screen.fill("black")
+        player.update(dt)    
+        
+        
+        
+        player.draw(screen)
+        
+        pygame.display.flip()
+        
 if __name__== "__main__":
     main()   
